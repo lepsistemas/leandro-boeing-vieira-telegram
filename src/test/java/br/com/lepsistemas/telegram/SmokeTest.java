54c9fbce560f23808461b744d1099ccb4afad4ea
Leandro Boeing Vieira <leandro.vieira@softplan.com.br>
2020-09-06 20:06:14 -0300

    Fix smoke test

--- src/test/java/br/com/lepsistemas/telegram/SmokeTest.java
+++ src/test/java/br/com/lepsistemas/telegram/SmokeTest.java
@@ -3,17 +3,12 @@ package br.com.lepsistemas.telegram;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class SmokeTest {
-	
-	@Value("${watson.api.key:dummyWatsonApiKey}")
-	private String watsonApiKey;
-	
-	@Value("${watson.assistant.id:dummyWatsonAssistantId}")
-	private String watsonAssistantId;
 	
 	@Test
 	void should_run() {
