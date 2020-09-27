9193967fcdb6865d873c264630e82ca5ff989802
Leandro Boeing Vieira <leandro.vieira@softplan.com.br>
2020-09-27 16:53:41 -0300

    Set profile to cloud on LogMongoDbFactory

--- src/main/java/br/com/lepsistemas/telegram/infrastructure/log/LogMongoDbFactory.java
+++ src/main/java/br/com/lepsistemas/telegram/infrastructure/log/LogMongoDbFactory.java
@@ -9,11 +9,13 @@ import org.apache.logging.log4j.mongodb3.LevelCodec;
 import org.apache.logging.log4j.mongodb3.MongoDbDocumentObject;
 import org.bson.codecs.configuration.CodecRegistry;
 import org.bson.codecs.pojo.PojoCodecProvider;
+import org.springframework.context.annotation.Profile;
 
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientOptions;
 import com.mongodb.MongoClientURI;
 
+@Profile("cloud")
 public class LogMongoDbFactory {
 	
 	private static final String CONNECTION_STRING = System.getenv("LOG_MONGODB_URI");
